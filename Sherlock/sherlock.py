63acaab683a1e0e1f163fa426ead3c770b329c6c
peterknights1 <peterk@knights.ucf.edu>
2017-08-19 10:05:58 -0400

    Update sherlock.py

--- Sherlock/sherlock.py
+++ Sherlock/sherlock.py
@@ -88,6 +88,19 @@ class Sherlock():
         self.DropHolesInData()
 
 
+    
+    def NameForecastDataColumn(self):
+        self.forecast_column = 'Adj. Close'
+
+
+
+    def ForecastOutOnePercentOfData(self, columnName):
+        self.forecast_column = columnName
+        # self.forecast_column = 'Adj. Close'
+        self.forecast_out = int(math.ceil(0.01 * len(self.data_frame)))
+        self.data_frame['label'] = self.data_frame[self.forecast_column].shift(-self.forecast_out)
+
+
 
     def CreateForecastDataColumn(self):
         self.forecast_column = 'Adj. Close'
@@ -106,6 +119,22 @@ class Sherlock():
         self.X = np.array(self.data_frame.drop(['label'], 1))
         # define y - labels, simply the label column of the dataframe, converted to numpy array
         self.y  = np.array(self.data_frame['label'])
+
+
+
+    # replacing above method
+    def GetXReadyForProcessing(self):
+        # define X - features as our entire dataframe EXCEPT for the label column (hence the drop)
+        self.X = np.array(self.data_frame.drop(['label'], 1))
+        self.X = preprocessing.scale(self.X)
+        self.X = self.X[:-self.forecast_out]
+        self.data_frame.dropna(inplace=True) # not sure if this should be part of this method here
+
+
+
+    def GetYReadyForProcessing(self):
+        self.y = np.array(self.data_frame['label'])
+
 
 
 
@@ -130,6 +159,12 @@ class Sherlock():
 
 
 
+    def TestConfidence(self):
+        self.confidence = self.clf.score(self.X_test, self.y_test)
+        return self.confidence
+
+
+
     # now ready to define the classifier - how to pick the best one? trial and error
     # if docs of classifier contain "n_jobs" means threading is supported 
     def UseSVRClassifier(self):
@@ -141,8 +176,9 @@ class Sherlock():
     # supports multithreading 
     def UseLinearRegressionClassifier(self):
         # self.clf = LinearRegression() # regular option
-        self.clf = LinearRegression(n_jobs = -1) # threading option
+        self.clf = LinearRegression(n_jobs=-1) # threading option
         self.TrainClassifier()
+
 
 
 
@@ -150,19 +186,94 @@ class Sherlock():
         for k in ['linear', 'poly', 'rbf', 'sigmoid']:
             self.clf = svm.SVR(kernel = k)
             self.TrainClassifier()
-            self.confidence = self.clf.score(self.X_test, self.y_test)
-            print(k, self.confidence)
+            # self.confidence = self.clf.score(self.X_test, self.y_test)
+            # print(k, self.confidence)
+            print(k, self.TestConfidence())
+        
+
+
+    def LitmusTest(self):
+        df = data.get("WIKI/GOOGL")
+        df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+        df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+        df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+        
+        df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+        forecast_col = 'Adj. Close'
+        df.fillna(value=-99999, inplace=True)
+        forecast_out = int(math.ceil(0.01 * len(df)))
+        df['label'] = df[forecast_col].shift(-forecast_out)
+        
+        X = np.array(df.drop(['label'], 1))
+        X = preprocessing.scale(X)
+        X = X[:-forecast_out]
+        df.dropna(inplace=True)
+        y = np.array(df['label'])
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+        
+        clf = LinearRegression(n_jobs=-1)
+        clf.fit(X_train, y_train)
+        confidence = clf.score(X_test, y_test)
+        print('\n\nGetting confidence of experiment B\n') 
+        print(confidence)
 
 
 
-    def TestConfidence(self):
-        self.confidence = self.clf.score(self.X_test, self.y_test)
-        print(self.confidence)
 
 
 
 
 
+    def Log(self):
+
+        sherlock = Sherlock()
+        self.talkamaton.Say('The game\'s afoot!')
+
+        sherlock.GetSampleData("WIKI/GOOGL")
+        sherlock.StripData()
+        
+        # create new data
+        sherlock.CalculateNewColumnBasedOnCrudeVolatility('HL_PCT')
+        sherlock.CalculateNewColumnBasedOnDailyPercentageChange('PCT_change')
+        sherlock.DefineNewDataFrame('Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume')
+
+        # clean data 
+        sherlock.FillHolesWithDummyPlaceholder()
+
+        # create the label - predictive data
+        sherlock.ForecastOutOnePercentOfData('Adj. Close') # again not sure about the order
+
+        # get ready in NumPy format for SciKit processing 
+        sherlock.GetXReadyForProcessing()
+        sherlock.GetYReadyForProcessing()
+        
+        # training and testing
+        sherlock.TrainingAndTestingOfFeaturesAndLabels()
+
+        # now can set classifiers
+        print('\n\nGetting confidence of experiment A - multiple classifiers\n') 
+        sherlock.UseMultipleClassifiers()
+        sherlock.TrainClassifier()
+
+
+        self.LitmusTest()
+
+        
+        self.talkamaton.Say('Excellent' + '...' + 'Elementary!' )
+
+         
+
+
+
+
+
+
+
+
+
+
+
+    ''' defunct log
     def Log(self):
         sherlock = Sherlock()
         self.talkamaton.Say('The game\'s afoot!' )
@@ -185,16 +296,46 @@ class Sherlock():
         sherlock.DefineNewDataFrame('Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume')
         sherlock.PrintDataHead()
 
+
+        # This order of operations might be problematic
         # starting on the prediction process of the 'label' based on the 'features'
         # Q: why create dummy place holders and then drop any remanining NaN? Ask
         sherlock.CreateForecastDataColumn()
         # sherlock.DropHolesInData()
         sherlock.CleanData()
         # sherlock.CreateForecastDataColumn() <-- get error when run this after clean data, with too many NaN values
+        
+
+        # name forecast column
+        # sherlock.NameForecastDataColumn() # preferrably should have name parameter
+
+        # fill na with -99999
+        sherlock.FillHolesWithDummyPlaceholder()
+
+        # forecast out 1 percent
+        # sherlock.ForecastOutOnePercentOfData()
+
+        # recheck order, if breaks 
+        sherlock.ForecastOutOnePercentOfData('Adj. Close') # again not sure about the order
+
+        # convert to numpy array
+        sherlock.GetXReadyForProcessing()
+        sherlock.GetYReadyForProcessing()
+
+        # testing and training
+        sherlock.TrainingAndTestingOfFeaturesAndLabels()
+
+        # now ready to define classifier
+        sherlock.UseMultipleClassifiers()
+        sherlock.TrainClassifier()
+
+        # check results 
+        sherlock.TestConfidence()
+
 
         print('\n\nTurning data into NumPy array format >>> >>>\n\n')
         sherlock.TurnDataIntoCompatibleNumpyArrayFormat()
-        # sherlock.ConvertDataToNegativeOnePlusOneRangeValues()
+        sherlock.ConvertDataToNegativeOnePlusOneRangeValues()
 
         sherlock.CreateLabelY()
         
@@ -216,18 +357,18 @@ class Sherlock():
         # print(self.confidence)
 
 
+        # review why code is off before moving on to part 5
+        # https://pythonprogramming.net/forecasting-predicting-machine-learning-tutorial/?completed=/training-testing-machine-learning-tutorial/
+
+
 
         self.talkamaton.Say('Excellent' + '...' + 'Elementary!' )
 
-
+        # defunct log '''
 
 
 sherlock = Sherlock()
 sherlock.Log()
-
-
-
-
 
 
 
