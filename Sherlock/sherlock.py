2c8b568192afa5a6a1ce5715e75b9b49a56af3c2
peterknights1 <peterk@knights.ucf.edu>
2017-08-19 19:49:00 -0400

    Update sherlock.py

--- Sherlock/sherlock.py
+++ Sherlock/sherlock.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import datetime
 
 import pandas as pd 
 import quandl as data
+data.ApiConfig.api_key = 'insert your api here'
+data.ApiConfig.api_version = '2015-04-09'
 
 import math
 import numpy as np
@@ -13,6 +16,13 @@ from sklearn.linear_model import LinearRegression
 
 # from includes.IBM.examples import text_to_speech_v1 once sign up for paid plan
 from includes import talkamaton
+
+
+import visualization
+visualization = visualization.Visualization()
+
+import matplotlib.pyplot as plt 
+from matplotlib import style
 
 
 
@@ -127,6 +137,7 @@ class Sherlock():
         # define X - features as our entire dataframe EXCEPT for the label column (hence the drop)
         self.X = np.array(self.data_frame.drop(['label'], 1))
         self.X = preprocessing.scale(self.X)
+        self.X_lately = self.X[-self.forecast_out:] # create another x with the most recent features we are going to predict against, notice colon is on other side
         self.X = self.X[:-self.forecast_out]
         self.data_frame.dropna(inplace=True) # not sure if this should be part of this method here
 
@@ -176,13 +187,14 @@ class Sherlock():
     # supports multithreading 
     def UseLinearRegressionClassifier(self):
         # self.clf = LinearRegression() # regular option
-        self.clf = LinearRegression(n_jobs=-1) # threading option
+        self.clf = LinearRegression(n_jobs=-1) # threading option, this is probably why we're getting different results
         self.TrainClassifier()
 
 
 
 
     def UseMultipleClassifiers(self):
+        # in the future, run through different classifiers and choose the one with the highest R score
         for k in ['linear', 'poly', 'rbf', 'sigmoid']:
             self.clf = svm.SVR(kernel = k)
             self.TrainClassifier()
@@ -190,6 +202,53 @@ class Sherlock():
             # print(k, self.confidence)
             print(k, self.TestConfidence())
         
+
+
+    def ForecastSet(self):
+        self.forecast_set = self.clf.predict(self.X_lately)
+        return self.forecast_set
+
+
+
+    def ForecastTable(self):
+        print(self.ForecastSet(), self.confidence, self.forecast_out)
+
+
+
+    def AddForecastColumn(self):
+        self.data_frame['Forecast'] = np.nan
+
+
+
+    def AssignEachNewForecastToEachNewDay(self):
+        last_date = self.data_frame.iloc[-1].name
+        last_unix = last_date.timestamp()
+        one_day = 86400
+        self.next_unix = last_unix + one_day
+
+
+
+    def AddForecastToExistingDataFrame(self):
+        for i in self.forecast_set:
+            next_date = datetime.datetime.fromtimestamp(self.next_unix)
+            self.next_unix += 86400
+            self.data_frame.loc[next_date] = [np.nan for _ in range(len(self.data_frame.columns)-1)]+[i]
+
+        # plotting them here for testing
+        self.data_frame['Adj. Close'].plot()
+        self.data_frame['Forecast'].plot()
+        plt.legend(loc=4)
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+        plt.show()
+
+
+    def Visual(self):
+        # visualiation.Plot(featureOne, featureTwo, legend, xLabel, yLabel)
+        visualization.Plot(self.data_frame['Adj. Close'], self.data_frame['Forecast'], 4, 'Date', 'Price')
+
+
+
 
 
     def LitmusTest(self):
@@ -252,14 +311,32 @@ class Sherlock():
 
         # now can set classifiers
         print('\n\nGetting confidence of experiment A - multiple classifiers\n') 
-        sherlock.UseMultipleClassifiers()
-        sherlock.TrainClassifier()
+        # sherlock.UseMultipleClassifiers() # use this one to test multiple ways
+        sherlock.UseLinearRegressionClassifier()
+        sherlock.TestConfidence()
+        # sherlock.TrainClassifier()
 
 
-        self.LitmusTest()
+        # self.LitmusTest()
 
-        
+
+        print('Forecast >>> \n\n')
+        # sherlock.ForecastTable()
+
+        sherlock.ForecastSet()
+        # creating the visual forecast on the chart
+        sherlock.AddForecastColumn()
+        sherlock.AssignEachNewForecastToEachNewDay()
+        sherlock.AddForecastToExistingDataFrame()
+
+        # finally get the visual 
+        # sherlock.Visual()
+      
+
+
+
         self.talkamaton.Say('Excellent' + '...' + 'Elementary!' )
+
 
          
 
@@ -373,7 +450,61 @@ sherlock.Log()
 
 
 
+import math
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime
 
+style.use('ggplot')
+
+df = data.get("WIKI/GOOGL")
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
 
 
 
